5200d3d0b50034892c5cb6bfd313da1301cbd63a
QQ <oven425@yahoo.com.tw>
2018-02-07 19:34:54 +0800

    First upload
    start up project

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_LogicSimulation
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            CQGateUI cc = null;
+            QGate ggate = null;
+            cc = new CQGateUI();
+            cc.GateName = "AND";
+            cc.Pin_in.Add(new CQPin());
+            cc.Pin_in.Add(new CQPin());
+            cc.Pin_out.Add(new CQPin());
+            ggate = new QGate();
+            ggate.Height = 50;
+            ggate.Width = 80;
+            ggate.DataContext = cc;
+            ggate.OnPinMouseDwon += Ggate_OnPinMouseDwon;
+            ggate.OnPinMouseUp += Ggate_OnPinMouseUp;
+            this.canvas.Children.Add(ggate);
+
+            cc = new CQGateUI();
+            cc.GateName = "AND";
+            cc.Pin_in.Add(new CQPin());
+            cc.Pin_in.Add(new CQPin());
+            cc.Pin_out.Add(new CQPin());
+            ggate = new QGate();
+            ggate.Height = 50;
+            ggate.Width = 80;
+            ggate.OnPinMouseDwon += Ggate_OnPinMouseDwon;
+            ggate.OnPinMouseUp += Ggate_OnPinMouseUp;
+            ggate.DataContext = cc;
+            this.canvas.Children.Add(ggate);
+        }
+        bool m_IsConnect;
+        Line m_Line;
+        private bool Ggate_OnPinMouseUp(CQPin pin, Point pt)
+        {
+            this.m_Line.X2 = pt.X;
+            this.m_Line.Y2 = pt.Y;
+            this.m_Line = null;
+            this.m_IsConnect = false;
+            return true;
+        }
+
+        private bool Ggate_OnPinMouseDwon(CQPin pin, Point pt)
+        {
+            this.m_Line = new Line();
+            this.m_Line.Fill = Brushes.Gray;
+            this.m_Line.X1 = this.m_Line.X2 = pt.X;
+            this.m_Line.Y1 = this.m_Line.Y2 = pt.Y;
+            this.m_Line.Stroke = Brushes.Gray;
+            this.m_Line.StrokeThickness = 1;
+            this.m_IsConnect = true;
+            this.canvas.Children.Add(this.m_Line);
+            return true;
+        }
+
+
+
+        bool m_IsDrag = false;
+        QGate m_DragGate;
+        Point m_DragOffset;
+        private void canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.Source is QGate)
+            {
+                this.m_DragGate = e.Source as QGate;
+                this.m_IsDrag = true;
+                this.m_DragOffset = e.GetPosition(this.m_DragGate);
+                this.canvas.CaptureMouse();
+            }
+        }
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(this.m_IsDrag == true)
+            {
+                Point pt = e.GetPosition(this.canvas);
+                Canvas.SetLeft(this.m_DragGate, pt.X - this.m_DragOffset.X);
+                Canvas.SetTop(this.m_DragGate, pt.Y - this.m_DragOffset.Y);
+            }
+            else if(this.m_IsConnect == true)
+            {
+                Point pt = e.GetPosition(this.canvas);
+                this.m_Line.X2 = pt.X-1;
+                this.m_Line.Y2 = pt.Y-1;
+            }
+        }
+
+        private void canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (this.m_IsDrag == true)
+            {
+                this.m_IsDrag = false;
+                this.canvas.ReleaseMouseCapture();
+            }
+        }
+    }
+}
---
+++ QGate.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace WPF_LogicSimulation
+{
+    /// <summary>
+    /// QGate.xaml 的互動邏輯
+    /// </summary>
+    public partial class QGate : UserControl
+    {
+        public delegate bool PinMouseDwonDelegate(CQPin pin, Point pt);
+        public event PinMouseDwonDelegate OnPinMouseDwon;
+        public delegate bool PinMouseUpDelegate(CQPin pin, Point pt);
+        public event PinMouseUpDelegate OnPinMouseUp;
+        public QGate()
+        {
+            InitializeComponent();
+        }
+
+        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Rectangle rectangle = sender as Rectangle;
+            CQPin ping = rectangle.DataContext as CQPin;
+            UIElement container = VisualTreeHelper.GetParent(this) as UIElement;
+            Point relativeLocation = rectangle.TranslatePoint(new Point(0, 0), container);
+            relativeLocation.X = relativeLocation.X + rectangle.Width;
+            relativeLocation.Y = relativeLocation.Y + rectangle.Height / 2;
+            if (this.OnPinMouseDwon != null)
+            {
+                this.OnPinMouseDwon(ping, relativeLocation);
+            }
+            e.Handled = true;
+        }
+
+        private void Rectangle_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Rectangle rectangle = sender as Rectangle;
+            CQPin ping = rectangle.DataContext as CQPin;
+            UIElement container = VisualTreeHelper.GetParent(this) as UIElement;
+            Point relativeLocation = rectangle.TranslatePoint(new Point(0, 0), container);
+            relativeLocation.X = relativeLocation.X + rectangle.Width / 2;
+            relativeLocation.Y = relativeLocation.Y + rectangle.Height / 2;
+            if (this.OnPinMouseUp != null)
+            {
+                this.OnPinMouseUp(ping, relativeLocation);
+            }
+            e.Handled = true;
+        }
+
+        private void Rectangle_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+
+    public class CQGateUI : INotifyPropertyChanged
+    {
+        public ObservableCollection<CQPin> Pin_in { set; get; }
+        public ObservableCollection<CQPin> Pin_out { set; get; }
+        public string GateName { set; get; }
+        public CQGateUI()
+        {
+            this.Pin_in = new ObservableCollection<CQPin>();
+            this.Pin_out = new ObservableCollection<CQPin>();
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        void Update(string name)
+        {
+            if(this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+
+    public class CQPin
+    {
+        public enum Types
+        {
+            IN,
+            OUT
+        }
+        public Types Type { set; get; }
+        public int Index { set; get; }
+    }
+}
